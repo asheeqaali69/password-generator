43c471835954c2a93f9bc973791e6820dfd35f8a
asheeqaali69 <asheeqaali27@gmail.com>
2025-03-15 20:17:06 +0500

    pushing code

---
+++ generator.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import random
+import string
+
+def generate_password(length, use_digits, use_special):
+    characters = string.ascii_letters
+
+    if use_digits:
+        characters += string.digits
+
+    if use_special:
+        characters += string.punctuation
+
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+st.title("Password Generator")
+
+length = st.slider("Select Length", min_value=6, max_value=32, value=12)
+
+use_digits = st.checkbox("Include Digits")
+
+use_special = st.checkbox("Include Special Charachters")
+
+if st.button("Generate Password"):
+    password = generate_password(length, use_digits, use_special)
+    st.write(f"Generated Password: `{password}`")
